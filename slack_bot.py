2127a87d9a43563818b0a7cf69879d891aa5741f
Adriano Vinhas <vinhas.adriano@gmail.com>
2017-01-31 22:29:41 +0000

    Slightly refactoring to the code to improve readability

--- slack_bot.py
+++ slack_bot.py
@@ -3,30 +3,37 @@ import datetime
 from slackclient import SlackClient
  
 BOT_TOKEN = "<TOKEN>"
-CHANNEL_NAME = "general"
+CHANNEL_ID = "C3XF3Q285"
+USER_ID = "U3YUCC5AN"
  
+def send_message(sc, message):
+    sc.api_call("chat.postMessage", channel=CHANNEL_ID, text=message, username="ted_chat")
+
+def bot_reaction(sc, message, user):
+    """
+    The function that you need to change to do what you want.
+    Keyword arguments:
+    sc -- the slack client instance that you use to make the API calls
+    message -- the message that was sent
+    user -- the user that sent the message
+    """
+    send_message(sc, "I'm alive!!!")
+
 def main():
     # Create the slackclient instance
     sc = SlackClient(BOT_TOKEN)
  
     # Connect to slack
     if sc.rtm_connect():
-        # Send first message
-        # sc.api_call("chat.postMessage",channel="C3XF3Q285",text="I'm ALIVE!!",username="ted_chat")        
- 
         while True:
-            # print(sc.api_call("channels.replies",channel="C3XF3Q285", thread_ts=time.time()))
             # Read latest messages
             for slack_message in sc.rtm_read():
                 message = slack_message.get("text")
                 user = slack_message.get("user")
                 if not message or not user:
                     continue
-                if user != 'U3YUCC5AN':
-                    sc.api_call("chat.postMessage",
-                                channel="C3XF3Q285",
-                                text="I'm ALIVE!!",
-                                username="ted_chat")
+                if user != USER_ID: # The bot only reacts to messages that are not coming from itself
+                    bot_reaction(sc, message, user)
             # Sleep for half a second
             time.sleep(0.5)
 
